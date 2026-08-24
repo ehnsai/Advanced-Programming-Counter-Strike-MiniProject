8b4714473c6eea10722fb97283f6fa26a4d59c01
ehnsai <ehsansalehi467@gmail.com>
2026-08-24 20:54:15 +0330

    Add files via upload

---
+++ src/miniproject.cpp
@@ -0,0 +1,47 @@
+﻿#include "Header_of_Gun.h"
+#include "Header_of_player.h"
+#include "Header_of_menu.h"
+#include "Header_of_ct.h"
+#include "Header_of_terrorist.h"
+#include "Header_of_random.h"
+#include "Header_of_testmanager.h"
+
+
+int main()
+{
+	menu menu;
+	menu.showMainMenu();
+}
+
+
+
+
+//int main() {
+//	TestManager tester;
+//	tester.runAllTests();
+//	return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	player p;
+//	p.sett_nameOFplayer("Test Player");
+//	Gun g1(1, 10, 30, 10,20);
+//	Gun g2(2, 11, 25, 8,30);
+//	Gun g3(3, 12, 5, 50,60);
+//
+//	p.AddGun(g1);
+//	p.AddGun(g2);
+//	p.AddGun(g3);
+//	p.navigateGuns();
+//
+//	return 0;
+//}
+
+
+
+
